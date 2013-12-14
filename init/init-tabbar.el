543c9413efa3c207d926584b36f4f7ba6ac797b4
exsaber <1960028060@qq.com>
2013-12-14 10:32:15 +0800

    remove tabbar-ruller

--- init/init-tabbar.el
+++ init/init-tabbar.el
@@ -1,9 +1,12 @@
 (require 'tabbar)
 
-(setq tabbar-ruler-global-tabbar 't) ; If you want tabbar
+
+(tabbar-mode 1)
+
+;(setq tabbar-ruler-global-tabbar 't) ; If you want tabbar
 ;(setq tabbar-ruler-global-ruler 't) ; if you want a global ruler
-(setq tabbar-ruler-popup-menu 't) ; If you want a popup menu.
-(setq tabbar-ruler-popup-toolbar 't) ; If you want a popup toolbar
-(require 'tabbar-ruler)
+;(setq tabbar-ruler-popup-menu 't) ; If you want a popup menu.
+;(setq tabbar-ruler-popup-toolbar 't) ; If you want a popup toolbar
+;(require 'tabbar-ruler)
 
 (provide 'init-tabbar)
